560733e512657f2a57a7810b719e6e0aeaef2770
runs <runs@runs-PC.runs.es>
2020-07-22 05:30:57 +0200

    first commit

--- potion_crafts.lua
+++ potion_crafts.lua
@@ -1,18 +1,15 @@
 local modpath, S = ...
 
--- Effects: antigrav, speed, ouhealth, health, air, ouair
-
 brewing.register_potion_craft({
 	effect= "jumping",
 	type= "add",
-	level= 1,
-	recipe = {'flowers:tulip', '', ''}
+	level= 2,
+	recipe = {'brewing:orange_mycena', 'brewing:cortinarius_violaceus', 'brewing:gliophorus_viridis'}
 })
 
---jumping_add1", recipe = { {'flowers:tulip','brewing:gerbera_oil','brewing:orange_mycena'}, {'','',''} } }
---"brewing:speed_add1", recipe = { {'flowers:dandelion_white','brewing:yellow_bell','flowers:geranium'}, {'','',''} } }
---"brewing:antigravity_add1", recipe = { {'brewing:mandragora','flowers:dandelion_white','brewing:star_anise'}, {'','',''} } }
---"brewing:air_add1", recipe = { {'flowers:rose','brewing:yellow_bell','brewing:gerbera_daisy'}, {'','',''} } }
---"brewing:ouair_add1", recipe = { {'flowers:dandelion_white','brewing:yellow_bell','flowers:tulip'}, {'','',''} } }
---"brewing:health_add1", recipe = { {'flowers:dandelion_white','brewing:saffron','brewing:star_anise'}, {'','',''} } }
---"brewing:ouhealth_add1"
+brewing.register_potion_craft({
+	effect= "health",
+	type= "add",
+	level= 2,
+	recipe = {'brewing:pluteus_chrysophaeus', 'brewing:leaiana_mycena', 'brewing:green_hygrocybe'}
+})
